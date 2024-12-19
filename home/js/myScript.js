b888872850d89f412e31dd6fc5173bca1fcc434c
Charlemagne De Guzman <csdeguzman@student.apc.edu.ph>
2024-12-19 23:30:48 +0800

    added another HTML DOM Function

--- home/js/myScript.js
+++ home/js/myScript.js
@@ -1,3 +1,9 @@
 function myFunction() {
     document.getElementById("demo").innerHTML="Paragraph changed.";
 }
+
+function myFunction2() {
+    const element = document.getElementsByTagName("div");
+ 
+document.getElementById("demo").innerHTML = 'The text in first <div> tag (index 0) is: ' + element[0].innerHTML;
+}
